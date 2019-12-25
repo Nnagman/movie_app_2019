8d8fe8a46576d15935eb75acbcab25fe33e82a4a
Nangman <ckdgh930314@naver.com>
2019-12-25 15:13:20 +0900

    Updates

---
+++ precache-manifest.0748bf2ca9d08a67e034c8a269854df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8e80d40a82a14b0a7ab745dca8a137",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e97cae13995d82135c9f",
+    "url": "/movie_app_2019/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "8526568d949b0ffebe66",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e97cae13995d82135c9f",
+    "url": "/movie_app_2019/static/js/main.9ec760fd.chunk.js"
+  },
+  {
+    "revision": "101d7acd549509bcf93b",
+    "url": "/movie_app_2019/static/js/runtime-main.69d003e7.js"
+  }
+]);
